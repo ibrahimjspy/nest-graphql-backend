12ce1b1f9adbd9ea4a01bdab41645c04ba5cb391
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-06-23 17:35:00 +0500

    Refactor: Minor changes in data filtering case : Moving logic from handler to generator function .

--- src/handlers/productCardHandler.ts
+++ src/handlers/productCardHandler.ts
@@ -1,4 +1,5 @@
 import { request } from 'graphql-request';
+import { isEmpty } from 'rxjs';
 import { mockProductCard } from 'src/queries/mock';
 // import { productCardQuery } from '../queries/productCard';
 
@@ -10,22 +11,23 @@ export const productCardHandler = async () => {
     'https://faker.graphqleditor.com/ibrahim/mockcardapi/graphql',
     mockProductCard(),
   ).then((data) => {
-    // demo data filtering for mock api
-    data.color_variant = ['red', 'blue', 'green', 'orange', 'black'];
-    data.image = 'https://iili.io/httHog.png';
-    data.title = 'Product title';
     const Data = dummyDataGenerator(data);
     productCardsData = Data;
   });
   return productCardsData;
 };
 
-// Mock query handler for testing
+// Data filtering of Mock Api
 
 const dummyDataGenerator = (data: JSON) => {
   const productCardsDataMock = { products: [] };
   for (let i = 0; i < 10; i++) {
     productCardsDataMock.products.push(data);
   }
+  productCardsDataMock.products.map((item) => {
+    item.title = 'Product title';
+    item.image = 'https://iili.io/httHog.png';
+    item.color_variant = ['red', 'blue', 'green', 'orange', 'black'];
+  });
   return productCardsDataMock;
 };
