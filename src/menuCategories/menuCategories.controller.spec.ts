c4a2094b2fa1e255106afb767819d876b93f5d9e
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-06-29 12:19:34 +0500

    Testing: MenuCategories tests implementation .

---
+++ src/menuCategories/menuCategories.controller.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { MenuCategoriesController } from './menuCategories.controller';
+import { MenuCategoriesService } from './menuCategories.service';
+
+//  Product card unit tests using Jest
+//  WARN!! Replace env configs with test links before running command npm run test
+
+describe('Categories controller', () => {
+  let appController: MenuCategoriesController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
+      controllers: [MenuCategoriesController],
+      providers: [MenuCategoriesService],
+    }).compile();
+
+    appController = app.get<MenuCategoriesController>(MenuCategoriesController);
+  });
+
+  // checking for values that are falsy and undefined --->>
+
+  describe('root', () => {
+    it('should return "JSON', () => {
+      expect(appController.findAll()).toBeDefined();
+    });
+    it('should not be falsy', () => {
+      expect(appController.findAll()).toBeTruthy();
+    });
+    it('the data is an object returned from graphQL', async () => {
+      const expected = { foo: 'bar' };
+      const data = await appController.findAll();
+      expect(data).toEqual(expect.not.objectContaining(expected));
+    });
+  });
+});
