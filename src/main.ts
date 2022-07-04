3f01f53cea529148745ac8c424755a8ab57a3893
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-07-04 16:06:04 +0500

    fix: CORS origin settings updated , "env config file for cors point"

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // global cors allowed
+  // app.enableCors();
+  // specific cors setting
+  app.enableCors({
+    origin: process.env.CORS_ORIGINS.split(','),
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
