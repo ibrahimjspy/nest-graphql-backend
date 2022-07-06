43278bfe3146cff581728ff8d0c758094865ada4
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-07-06 10:43:06 +0500

    Implementation : ProductCard randomisation and image configuration .

--- src/handlers/productCard/Handler.ts
+++ src/handlers/productCard/Handler.ts
@@ -8,7 +8,16 @@ export const productCardHandler = async () => {
   const GRAPHQL_ENDPOINT: string = process.env.MOCK_GRAPHQL_ENDPOINT;
   let productCardsData = {};
   await request(GRAPHQL_ENDPOINT, mockProductCard()).then((data) => {
+    shuffleArray(data.products);
     productCardsData = data;
   });
   return productCardsData;
 };
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
