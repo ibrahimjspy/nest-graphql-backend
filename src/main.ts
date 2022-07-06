28db03c1e8b3083785aa7071ad55f535d2407f5f
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-07-06 10:19:18 +0500

    Update:Cors global update .

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // global cors allowed
-  // app.enableCors();
+  app.enableCors();
   // specific cors setting
-  app.enableCors({
-    origin: process.env.CORS_ORIGINS.split(','),
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: process.env.CORS_ORIGINS.split(','),
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  //   credentials: true,
+  // });
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
