a5fee2ff2554cdc8275d1fdbf681d9a754efa46f
umairbashir21 <umair.bashir@aiworks.ai>
2022-09-09 12:14:53 +0500

    Update: cors permissions updated

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CORS_ORIGINS.split(','),
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 5000);
 }
 
