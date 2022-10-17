60df1518236a5986c34150500629a6faec750c9b
Sheikh Haris Zahid <106780114+shz-aiworks@users.noreply.github.com>
2022-10-17 16:39:31 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import packageInfo from '../package.json';
 
-const corsOrigins = async () => process.env.CORS_ORIGINS.split(',');
+const corsOrigins = async () => (process.env.CORS_ORIGINS || '').split(',');
 
 const corsMethods = async () =>
   (process.env.CORS_METHODS || 'GET,PATCH,POST,PUT,DELETE,OPTIONS')
@@ -14,11 +14,15 @@ const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
 
   // cores configuration
-//   app.enableCors({
-//     origin: await corsOrigins(),
-//     methods: await corsMethods(),
-//     credentials: true,
-//   });
+  // FIXME: need to properly configure the cors.
+  app
+    .enableCors
+    //   {
+    //   origin: await corsOrigins(),
+    //   methods: await corsMethods(),
+    //   credentials: true,
+    // }
+    ();
 
   // swagger configuration
   const config = new DocumentBuilder()
