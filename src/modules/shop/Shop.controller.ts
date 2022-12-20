4c389cb9bd7d5aadcf4dba3c4823fd880616c83f
umairbashir21 <umair.bashir@aiworks.ai>
2022-12-20 13:05:53 +0500

    feature/SHR-792: get shop details api

--- src/modules/shop/Shop.controller.ts
+++ src/modules/shop/Shop.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Headers } from '@nestjs/common';
+import { Controller, Get, Headers, Param, Res } from '@nestjs/common';
+import { makeResponse } from '../../core/utils/response';
 import { ApiTags } from '@nestjs/swagger';
 import { ShopService } from './Shop.service';
 
@@ -11,5 +12,13 @@ export class ShopController {
   findBanner(@Headers() headers): Promise<object> {
     const Authorization: string = headers.authorization;
     return this.appService.getCarouselData(Authorization);
+  }
+
+  @Get('/api/v1/details/:shopId')
+  async getShippingMethods(@Res() res, @Param() params): Promise<object> {
+    return makeResponse(
+      res,
+      await this.appService.getShopDetails(params?.shopId),
+    );
   }
 }
