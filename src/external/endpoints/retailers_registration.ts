dacba6e7098d019917cfa7eea6d1342e67c8396b
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-12-23 11:46:21 +0500

    feature/SHR-952:code refactored

--- src/external/endpoints/retailers_registration.ts
+++ src/external/endpoints/retailers_registration.ts
@@ -1,13 +1,8 @@
 import http from 'src/core/proxies/restHandler';
-import { BASE_EXTERNAL_ENDPOINT } from 'src/constants';
+import { BASE_EXTERNAL_ENDPOINT, COMMON_HEADERS } from 'src/constants';
 
 export const retailerJobTitles = async () => {
   const URL = `${BASE_EXTERNAL_ENDPOINT}/api/v3/app/job-title`;
-  const response = await http.get(URL, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept-Encoding': 'application/json',
-    },
-  });
+  const response = await http.get(URL, COMMON_HEADERS);
   return response;
 };
