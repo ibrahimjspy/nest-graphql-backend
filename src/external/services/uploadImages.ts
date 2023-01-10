f4ec0b14f64908fe04b62599e7f06014f6fe554f
umairbashir21 <umair.bashir@aiworks.ai>
2023-01-10 16:14:14 +0500

    feature/SHR-1025: env added

--- src/external/services/uploadImages.ts
+++ src/external/services/uploadImages.ts
@@ -1,14 +1,11 @@
 import { uploadImages } from 'src/external/endpoints/uploadImages';
-import {
-  prepareFailedResponse,
-  prepareSuccessResponse,
-} from 'src/core/utils/response';
+import { prepareFailedResponse } from 'src/core/utils/response';
 import { getHttpErrorMessage } from 'src/external/utils/httpHelper';
 
 export const uploadImagesHandler = async (file: any) => {
   try {
     const resp = await uploadImages(file);
-    return prepareSuccessResponse(resp);
+    return resp;
   } catch (error) {
     const error_obj = getHttpErrorMessage(error);
     return prepareFailedResponse(error_obj?.message?.data);
