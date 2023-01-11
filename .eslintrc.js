baa446a6b09f9824719e64d74f06fa92c14e5c24
umairbashir21 <umair.bashir@aiworks.ai>
2023-01-11 16:24:30 +0500

    feature/SHR-1060: build error fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-unsafe-optional-chaining': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
     'sort-imports': [
       'error',
       {
