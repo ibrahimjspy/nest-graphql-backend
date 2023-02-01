fe7752d51fab86e622204699de48066dd1c5ac42
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-02-01 10:55:00 +0500

    Update : removed edges from categories

--- src/graphql/handlers/categories.ts
+++ src/graphql/handlers/categories.ts
@@ -7,7 +7,7 @@ import { menuCategoriesQuery } from 'src/graphql/queries/categories/menu';
 export const menuCategoriesHandler = async (): Promise<object> => {
   try {
     const response = await graphqlCall(menuCategoriesQuery());
-    return response['categories'];
+    return response?.categories?.edges?.slice(1);
   } catch (error) {
     return graphqlExceptionHandler(error);
   }
