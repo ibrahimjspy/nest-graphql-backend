c9be241d7d4c903dffb9b8f4675d23e8d3103441
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-02-01 10:26:22 +0500

    Fix : categories filter remove

--- src/graphql/handlers/categories.ts
+++ src/graphql/handlers/categories.ts
@@ -2,13 +2,12 @@ import {
   graphqlCall,
   graphqlExceptionHandler,
 } from 'src/core/proxies/graphqlHandler';
-import { categoriesFilter } from 'src/core/utils/categoryFilter';
 import { menuCategoriesQuery } from 'src/graphql/queries/categories/menu';
 
 export const menuCategoriesHandler = async (): Promise<object> => {
   try {
-    const categories = await graphqlCall(menuCategoriesQuery());
-    return categoriesFilter(categories);
+    const response = await graphqlCall(menuCategoriesQuery());
+    return response['categories'];
   } catch (error) {
     return graphqlExceptionHandler(error);
   }
