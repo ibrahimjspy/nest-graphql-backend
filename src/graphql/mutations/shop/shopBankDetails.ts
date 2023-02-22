80a175c58e2204b9a767c25bbd1a657c1cc0c63f
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-02-22 12:58:17 +0500

    Update : shop bank details queries

--- src/graphql/mutations/shop/shopBankDetails.ts
+++ src/graphql/mutations/shop/shopBankDetails.ts
@@ -2,16 +2,18 @@ import { gql } from 'graphql-request';
 
 export const shopBankDetailsMutation = (shopId: string, accountId: string) => {
   return gql`
-  mutation{
-    createShopAccountDetails(
-      input: {
-        accountId: "${accountId}"
-        shopId: "${shopId}"
+    mutation {
+      createShopAccountDetails(input: { shopId: "${shopId}", accountId: "${accountId}" }) {
+        __typename
+        ... on ShopBankAccountType {
+          shop
+          id
+          accReferId
+        }
+        ... on ResultError {
+          message
+        }
       }
-    )
-    {
-      accReferId
     }
-  }
   `;
 };
--- src/graphql/queries/shop/shopBankDetailsQuery.ts
+++ src/graphql/queries/shop/shopBankDetailsQuery.ts
@@ -2,11 +2,17 @@ import { gql } from 'graphql-request';
 
 export const shopBankDetailsQuery = (shopId: string): string => {
   return gql`
-  query{
-    shopBankDetails(input: {shopId: "${shopId}"})
-     {
-       accReferId
-     }
-   }
+    query {
+      shopBankDetails(input: { shopId: "${shopId}" }) {
+        ... on ShopBankAccountType {
+          id
+          shop
+          accReferId
+        }
+        ... on ResultError {
+          message
+        }
+      }
+    }
   `;
 };
