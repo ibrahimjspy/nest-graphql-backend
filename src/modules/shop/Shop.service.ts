650234a6821901ec9bc06d17e64c8e2539bf9429
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-02-22 15:05:56 +0500

    Update : provision storefront url transformer update

--- src/modules/shop/Shop.service.ts
+++ src/modules/shop/Shop.service.ts
@@ -335,7 +335,8 @@ export class ShopService {
   public generateStorefrontUrl(storeName: string) {
     try {
       const uniqueString = (Math.random() + 1).toString(36).substring(7); //e.g ~~ jce4r
-      const subDomain = `${storeName.replace(/\s+/g, '')}${uniqueString}`;
+      const validateStoreName = storeName.replace(/\s+/g, '').toLowerCase();
+      const subDomain = `${validateStoreName}${uniqueString}`;
       const url = `${subDomain}${B2C_STOREFRONT_TLD}`;
       return url;
     } catch (error) {
