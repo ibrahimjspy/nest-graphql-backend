16f1439275f4d6b56b9c6b1fb39b5b1a1d88e5ef
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-03-03 18:09:56 +0500

    Update : rollback for delete bundles from cart

--- src/modules/checkout/cart/services/Rollback.service.ts
+++ src/modules/checkout/cart/services/Rollback.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { SaleorCartService } from './saleor/Cart.saleor.service';
 import { MarketplaceCartService } from './marketplace/Cart.marketplace.service';
 import { graphqlExceptionHandler } from 'src/core/proxies/graphqlHandler';
+import { CheckoutBundleInputType } from 'src/graphql/handlers/checkout.type';
 
 @Injectable()
 export class CartRollbackService {
@@ -44,6 +45,48 @@ export class CartRollbackService {
       return await this.saleorCartService.deleteLines(
         checkoutId,
         lineIds,
+        token,
+      );
+    } catch (error) {
+      this.logger.error(error);
+      return graphqlExceptionHandler(error);
+    }
+  }
+
+  public async deleteCheckoutBundlesMarketplace(
+    userEmail,
+    checkoutId,
+    bundlesList,
+    token,
+  ) {
+    try {
+      this.logger.warn(
+        `Rolling back Saleor lines deleted due to failure in Marketplace against checkout :: ${checkoutId}`,
+      );
+      return await this.saleorCartService.addBundleLines(
+        userEmail,
+        checkoutId,
+        bundlesList,
+        token,
+      );
+    } catch (error) {
+      this.logger.error(error);
+      return graphqlExceptionHandler(error);
+    }
+  }
+
+  public async deleteCheckoutBundleLinesSaleor(
+    userEmail: string,
+    bundlesList: CheckoutBundleInputType[],
+    token: string,
+  ) {
+    try {
+      this.logger.warn(
+        `Rolling back Marketplace bundles deleted due to failure in Saleor against user :: ${userEmail}`,
+      );
+      return await this.marketplaceCartService.addBundles(
+        userEmail,
+        bundlesList,
         token,
       );
     } catch (error) {
