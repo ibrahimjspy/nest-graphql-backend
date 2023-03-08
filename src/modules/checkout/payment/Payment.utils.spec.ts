52cf14c9b0a43bd758597a42cfeeb7090a0f3baf
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-03-08 12:30:02 +0500

    Updated: added utils for payment

--- src/modules/checkout/payment/Payment.utils.spec.ts
+++ src/modules/checkout/payment/Payment.utils.spec.ts
@@ -1,4 +1,7 @@
-import { getPaymentIntentFromMetadata } from './Payment.utils';
+import {
+  getPaymentIntentFromMetadata,
+  paymentIntentAmountValidate,
+} from './Payment.utils';
 
 describe('Payment utilities unit test', () => {
   it('testing whether payment intent id is correctly getting parsed from checkout metadata', async () => {
@@ -7,5 +10,17 @@ describe('Payment utilities unit test', () => {
     console.log(paymentIntentId);
     expect(paymentIntentId).toBeDefined();
     expect(paymentIntentId).toStrictEqual('testIntentId');
+  });
+
+  it('testing whether payment intent amount is correctly getting compared with checkout amount', async () => {
+    const checkoutAmount = 2013;
+    const paymentIntentAmount = 2002;
+    const isAmountNew = paymentIntentAmountValidate(
+      checkoutAmount,
+      paymentIntentAmount,
+    );
+    console.log(isAmountNew);
+    expect(isAmountNew).toBeDefined();
+    expect(isAmountNew).toStrictEqual(false);
   });
 });
