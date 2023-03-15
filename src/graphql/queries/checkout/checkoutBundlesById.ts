d88845a30bb0cfa7165495a4acf2b064b068be48
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-03-15 11:56:35 +0500

    Update : price for checkout bundles

--- src/graphql/queries/checkout/checkoutBundlesById.ts
+++ src/graphql/queries/checkout/checkoutBundlesById.ts
@@ -19,6 +19,7 @@ export const checkoutBundlesByIdQuery = (
             checkoutBundleId
             isSelected
             quantity
+            price
             bundle {
               id
               name
