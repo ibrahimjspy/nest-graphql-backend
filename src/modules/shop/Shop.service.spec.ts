9728d05b44a17f4791f17f998bef682506ab07e5
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-05-09 16:26:06 +0500

    Implementation: shop service integration tests

--- src/modules/shop/Shop.service.spec.ts
+++ src/modules/shop/Shop.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as ShopHandlers from 'src/graphql/handlers/shop';
+import * as ProductHandlers from 'src/graphql/handlers/product';
+
 import * as Github from 'src/external/endpoints/provisionStorefront';
 
 import { ShopService } from './Shop.service';
 import { ShopType } from 'src/graphql/types/shop.type';
+import { shopIntegrationMocks } from '../../../test/mock/shop';
 
 describe('Shop Service Integration test', () => {
   let service: ShopService;
+  const mocks = shopIntegrationMocks;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,7 +21,7 @@ describe('Shop Service Integration test', () => {
     service = module.get<ShopService>(ShopService);
   });
 
-  it('should get all shops', async () => {
+  it('should get shops data', async () => {
     jest
       .spyOn(ShopHandlers, 'getShopDetailsV2Handler')
       .mockImplementation(async () => {
@@ -87,5 +91,36 @@ describe('Shop Service Integration test', () => {
       message: 'new storefront provisioned',
     });
     expect(createStore).toBeDefined();
+  });
+
+  it('get my products api', async () => {
+    jest
+      .spyOn(ShopHandlers, 'getShopDetailsV2Handler')
+      .mockImplementation(async () => mocks.mockShop);
+    jest
+      .spyOn(ProductHandlers, 'getShopProductsHandler')
+      .mockImplementation(async () => mocks.mockShopProducts);
+    jest
+      .spyOn(ProductHandlers, 'getMyProductsHandler')
+      .mockImplementation(async () => mocks.mockMyProducts);
+
+    const getMyProducts = await service.getMyProducts('123', { first: 12 });
+    expect(getMyProducts).toEqual(mocks.expectedMyProductsResponse);
+    expect(getMyProducts).toBeDefined();
+  });
+
+  it('should update shops', async () => {
+    jest
+      .spyOn(ShopHandlers, 'updateStoreInfoHandler')
+      .mockImplementation(async () => {
+        return { shop: 'test', url: '123.com' };
+      });
+
+    const getShop = await service.updateStoreInfo('97', { url: '123.com' }, '');
+    expect(getShop).toEqual({
+      status: 200,
+      data: { shop: 'test', url: '123.com' },
+    });
+    expect(getShop).toBeDefined();
   });
 });
