2ca2ea6bbfea40d6c8e2b22e76781fc7843b1b22
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-06-16 17:50:03 +0500

    Update: preorder for checkout complete

--- src/graphql/queries/checkout/checkoutBundlesById.ts
+++ src/graphql/queries/checkout/checkoutBundlesById.ts
@@ -52,6 +52,9 @@ export const checkoutBundlesByIdQuery = (
                   id
                   name
                   sku
+                  preorder {
+                    globalThreshold
+                  }
                   media {
                     url
                   }
